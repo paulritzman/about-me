79e7208811a7b4232235cabc125a6fe64f2934c7
paulritzman <ritzman.paul@gmail.com>
2018-04-04 18:23:19 -0700

    completed for-loop and if-statement logic; updated some messaging in alerts; removed commented-out legacy code

--- js/app.js
+++ js/app.js
@@ -1,101 +1,5 @@
 'use strict';
 
-/*
-
-var totalScore = 0; // Declare a totalScore variable to track number of correct responses.
-
-// Prompts the user to answer a question, logging the user's answer and adjusting
-// totalScore, if necessary. Correct answer = 'Y' or 'YES'
-var sportsQ = prompt('Has Paul competed in over 5 different sports? (Y/N)').toUpperCase();
-console.log('sportsQ - user answered:', sportsQ);
-
-if (sportsQ === 'Y' || sportsQ === 'YES') {
-  alert('Correct! Paul has actually competed in 9 different sports.');
-  totalScore++;
-} else if (sportsQ === 'N' || sportsQ === 'NO') {
-  alert('Incorrect, Paul has competed in 9 different sports.');
-} else {
-  alert('Please only answer with Yes or No.');
-}
-
-console.log('totalScore =', totalScore); // Output the current score to the console.
-
-// Prompts the user to answer a question, logging the user's answer and adjusting
-// totalScore, if necessary. Correct answer = 'Y' or 'YES'
-var cehQ = prompt('Is Paul a "Certified Ethical Hacker" (CEH)? (Y/N)').toUpperCase();
-console.log('cehQ - user answered:', cehQ);
-
-if (cehQ === 'Y' || cehQ === 'YES') {
-  alert('Correct! Paul got his CEH ceritification in August 2016.');
-  totalScore++;
-} else if (cehQ === 'N' || cehQ === 'NO') {
-  alert('Incorrect. Paul got his CEH cerification in August 2016');
-} else {
-  alert('Please only answer with Yes or No.');
-}
-
-console.log('totalScore =', totalScore); // Output the current score to the console.
-
-// Prompts the user to answer a question, logging the user's answer and adjusting
-// totalScore, if necessary. Correct answer = 'N' or 'NO'
-var pizzaToppingQ = prompt('Are pineapples Paul\'s favorite pizza topping? (Y/N)').toUpperCase();
-console.log('pizzaToppingQ - user answered:', pizzaToppingQ);
-
-if (pizzaToppingQ === 'Y' || pizzaToppingQ === 'YES') {
-  alert('Incorrect. Pineapples are fine on pizza, but not on Paul\'s pizza.');
-} else if (pizzaToppingQ === 'N' || pizzaToppingQ === 'NO') {
-  alert('Correct! Pineapples are fine on pizza, but not on Paul\'s pizza.');
-  totalScore++;
-} else {
-  alert('Please only answer with Yes or No.');
-}
-
-console.log('totalScore =', totalScore); // Output the current score to the console.
-
-// Prompts the user to answer a question, logging the user's answer and adjusting
-// totalScore, if necessary. Correct answer = 'N' or 'NO'
-var fatherQ = prompt('Did Paul\'s father serve in the U.S. Navy? (Y/N)').toUpperCase();
-console.log('fatherQ - user answered:', fatherQ);
-
-if (fatherQ === 'Y' || fatherQ === 'YES') {
-  alert('Incorrect. Paul\'s father was in the Air Force.');
-} else if (fatherQ === 'N' || fatherQ === 'NO') {
-  alert('Correct! Paul\'s father was in the Air Force.');
-  totalScore++;
-} else {
-  alert('Please only answer with Yes or No.');
-}
-
-console.log('totalScore =', totalScore); // Output the current score to the console.
-
-// Prompts the user to answer a question, logging the user's answer and adjusting
-// totalScore, if necessary. Correct answer = 'Y' or 'YES'
-var emeraldQ = prompt('Is Paul\'s birthstone: emerald? (Y/N)').toUpperCase();
-console.log('emeraldQ - user answered:', emeraldQ);
-
-if (emeraldQ === 'Y' || emeraldQ === 'YES') {
-  alert('Correct! Paul was born in May.');
-  totalScore++;
-} else if (emeraldQ === 'N' || emeraldQ === 'NO') {
-  alert('Incorrect. Paul was born in May.');
-} else {
-  alert('Please only answer with Yes or No.');
-}
-
-var randomNumQ = prompt('Placeholder text');
-console.log('randomNumQ - user answered', randomNumQ);
-
-var stateGuessQ = prompt('Placeholder text');
-console.log('randomNumQ - user answered', stateGuessQ);
-
-// Output the final score to the console and notify user of their final score.
-console.log('totalScore =', totalScore);
-alert('You scored: ' + totalScore + '/5!');
-
-*/
-
-// ***** DO NOT TOUCH ANYTHING ABOVE THIS LINE - IT IS LEGACY CODE *****
-
 var questionArray = [
   'Has Paul competed in over 5 different sports? (Y/N)',
   'Is Paul a "Certified Ethical Hacker" (CEH)? (Y/N)',
@@ -124,21 +28,10 @@ var userAnswer;
 
 var userName = prompt('Please enter your first name.');
 
+alert('Let\'s start the Guessing Game!')
+
 for (var i = 0; i < questionArray.length; i++) {
-  if (i < 5) {
-    userAnswer = prompt(questionArray[i]).toUpperCase();
-    console.log('For question ' + i + ', the user answered: ' + userAnswer);
-
-    if (userAnswer === answerArray[i] || userAnswer === answerArray[i + 5]) {
-      alert('Correct! ' + responseArray[i]);
-      playerScore++;
-    } else if (userAnswer === wrongAnswerArray[i] || userAnswer === wrongAnswerArray[i + 5]) {
-      alert('Incorrect. ' + responseArray[i]);
-    } else {
-      alert('invalid response');
-    } // closes main, inner question if-else statement
-
-  } else if (i === 5) {
+  if (i === 5) {
     var randomNum = Math.floor(Math.random() * 10 + 1);
     var randomGuesses = 4;
 
@@ -159,6 +52,7 @@ for (var i = 0; i < questionArray.length; i++) {
 
       randomGuesses--;
     } while (userAnswer !== randomNum && randomGuesses > 0);
+
   } else if (i === 6) {
     var stateGuesses = 6;
 
@@ -168,7 +62,7 @@ for (var i = 0; i < questionArray.length; i++) {
       console.log('Guesses left = ' + (stateGuesses - 1));
 
       if (userAnswer === stateAnswers[0] || userAnswer === stateAnswers[1] || userAnswer === stateAnswers[2]) {
-        alert('Correct! All possible answer: ' + stateAnswers);
+        alert('Correct! All possible answers: ' + stateAnswers);
         playerScore++;
         break;
       } else {
@@ -176,8 +70,25 @@ for (var i = 0; i < questionArray.length; i++) {
       } // closes state if-else statement
 
       stateGuesses--;
+
+      if (stateGuesses === 0) {
+        alert('Sorry, those weren\'t correct. The three states Paul would like to move to are: ' + stateAnswers);
+      }
     } while ((userAnswer !== stateAnswers[0] || userAnswer !== stateAnswers[1] || userAnswer !== stateAnswers[2]) && stateGuesses > 0);
-  } // closes the "else-if (1 === 6)" block
+
+  } else {
+    userAnswer = prompt(questionArray[i]).toUpperCase();
+    console.log('For question ' + i + ', the user answered: ' + userAnswer);
+
+    if (userAnswer === answerArray[i] || userAnswer === answerArray[i + 5]) {
+      alert('Correct! ' + responseArray[i]);
+      playerScore++;
+    } else if (userAnswer === wrongAnswerArray[i] || userAnswer === wrongAnswerArray[i + 5]) {
+      alert('Incorrect. ' + responseArray[i]);
+    } else {
+      alert('Please only answer yes (Y) or no (N).');
+    } // closes else statement
+  } // closes entire if/else statement
 } // closes for-loop
 
 alert('You got ' + playerScore + ' of ' + questionArray.length + ' questions correct, ' + userName + '!');
